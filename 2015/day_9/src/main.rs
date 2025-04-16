f5af0425ae780e6f4888b8f052e54f21636e2383
Warren Wang <warren.wang2020@gmail.com>
2025-04-16 12:17:15 -0700

    2015: fin day 9

--- 2015/day_9/src/main.rs
+++ 2015/day_9/src/main.rs
@@ -1,8 +1,107 @@
-use std::fs;
+use itertools::Itertools;
+use std::{collections::HashMap, fs};
+
+fn is_valid_path(
+    path: Vec<&&&str>,
+    matrix: &Vec<Vec<i32>>,
+    nodes_map: &HashMap<&str, usize>,
+) -> Option<usize> {
+    let mut res: usize = 0;
+
+    for i in 0..path.len() - 1 {
+        let n1 = path[i];
+        let n2 = path[i + 1];
+
+        let j = nodes_map.get(**n1).unwrap();
+        let k = nodes_map.get(**n2).unwrap();
+        let edge = matrix[*j][*k];
+
+        if edge <= 0 {
+            return None;
+        }
+        res += edge as usize;
+    }
+
+    Some(res)
+}
 
 fn main() {
+    // shortest hamiltonian path problem
+    // the graph looks small enough for us to just brute force it
+    // as opposed to use an alg like held-karp
+    // 8 nodes -> 8! -> 40320
     let input = fs::read_to_string("input/9.txt").expect("file not found");
     let input: Vec<_> = input.trim().split("\n").collect();
+    // println!("{:?}", input);
 
-    // TODO: shortest hamiltonian path problem...
+    let mut nodes_map: HashMap<&str, usize> = HashMap::new();
+    let mut counter: usize = 0;
+
+    // get set of unqiue nodes in graph
+    for l in input.clone().into_iter() {
+        let parts: Vec<_> = l.split(" ").collect();
+        let x1 = parts[0];
+        let x2 = parts[2];
+
+        if !nodes_map.contains_key(x1) {
+            nodes_map.insert(x1, counter);
+            counter += 1;
+        }
+
+        if !nodes_map.contains_key(x2) {
+            nodes_map.insert(x2, counter);
+            counter += 1;
+        }
+    }
+    // init adjacency matrix (note that upper and lower triangular sections mirror
+    // each other on the main diagonal from top left to bottom right)
+    // store empty/invalid edge with -1
+    // store self to self edge as 0
+    let n = nodes_map.len();
+    let mut matrix = vec![vec![-1; n]; n];
+    for i in 0..n {
+        for j in 0..n {
+            if i == j {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    // add valid edges
+    for l in input.into_iter() {
+        let parts: Vec<_> = l.split(" ").collect();
+        let x1 = parts[0];
+        let x2 = parts[2];
+        let dist: i32 = parts[4].trim().parse().unwrap();
+
+        // println!("{} {} {}", x1, x2, dist);
+
+        let i = nodes_map.get(x1).unwrap();
+        let j = nodes_map.get(x2).unwrap();
+
+        matrix[*i][*j] = dist;
+        matrix[*j][*i] = dist;
+    }
+
+    for r in matrix.clone().iter() {
+        println!("{:?}", r);
+    }
+
+    // find the smallest and longest valid paths through all nodes
+    let mut smallest_path = 10000;
+    let mut longest_path = 0;
+
+    let keys: Vec<_> = nodes_map.keys().collect();
+    for perm in keys.iter().permutations(keys.len()) {
+        if let Some(valid_path_len) = is_valid_path(perm, &matrix, &nodes_map) {
+            if valid_path_len < smallest_path {
+                smallest_path = valid_path_len;
+            }
+            if valid_path_len > longest_path {
+                longest_path = valid_path_len;
+            }
+        }
+    }
+
+    println!("{}", smallest_path); // part one answer: 141
+    println!("{}", longest_path);
 }
