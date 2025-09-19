b265730ad59c85c45be93ace091bf9e1bd546d72
Warren Wang <warren.min.wang@gmail.com>
2025-09-18 22:15:58 -0700

    simplify 2015.4

--- src/year_2015/day_4.rs
+++ src/year_2015/day_4.rs
@@ -1,17 +1,24 @@
-fn mine(input: &str, num_leading_zeros: usize) -> i32 {
-    let desired_leading = "0".repeat(num_leading_zeros);
+pub fn day_4(input: &str) {
+    let mut found_5 = false;
+    let mut found_6 = false;
+
     let mut i = 0;
     let mut digest: String;
     loop {
         digest = format!("{:x}", md5::compute(format!("{}{}", input, i)));
-        if digest.starts_with(&desired_leading) {
-            return i;
+
+        if !found_5 && digest.starts_with("00000") {
+            println!("2015.4 Part 1: {}", i);
+            found_5 = true;
         }
+        if !found_6 && digest.starts_with("000000") {
+            println!("2015.4 Part 2: {}", i);
+            found_6 = true;
+        }
+        if found_5 && found_6 {
+            return;
+        }
+
         i += 1;
     }
-}
-
-pub fn day_4(input: &str) {
-    println!("2015.4 Part 1: {}", mine(input, 5));
-    println!("2015.4 Part 2: {}", mine(input, 6));
 }
